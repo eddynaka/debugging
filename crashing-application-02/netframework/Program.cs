bb00a4827fa30b71320a4885a0af888737708caf
Eddy Nakamura <eddynaka@gmail.com>
2020-08-16 11:09:28 -0300

    Adding crashing / memory leak application (#6)

---
+++ crashing-application-02/netframework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrashingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(60_000);
+            try
+            {
+                HttpRequest();
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"C:/Windows/private.txt", ex.ToString());
+            }
+        }
+
+        private static bool HttpRequest()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = httpClient.GetAsync("https://www.wronggithuburl.com").GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
